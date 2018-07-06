0aa10b683c4df62067278ff92289485a9a38e09a
kekonen <daniil.naumetc@gmail.com>
2018-07-06 11:55:21 +0200

    order complete

--- p2pTestSuit.js
+++ p2pTestSuit.js
@@ -11,8 +11,8 @@ const request = superagent.agent().redirects(20)
 // var tough = require('tough-cookie');
 
 
-const eprocLogin = 'Peter.Bedarf' //process.env.P2PLOGIN;
-const eprocPass  = 'Peter.Bedarf' //process.env.P2PPASS;
+const eprocLogin = process.env.P2PLOGIN;
+const eprocPass  = process.env.P2PPASS;
 
 
 var eproc = {
@@ -39,7 +39,6 @@ class CookieJar{
         if (typee == 'object') {
             if (Array.isArray(cookies)) {
                 cookies.forEach(cookie => {
-                    console.log('->',cookie)
                     var [cookieName, cookieValue] = cookie.split('=')
                     this.cookies[cookieName] = cookieValue
                 })
@@ -60,11 +59,7 @@ class CookieJar{
 }
 
 
-console.log(`
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////  Welcome to '${eproc.uri()}', login: '${eproc.login}', pass: '${eproc.pass}'
-///////////////////////////////////////////////////////////////////////////////////////////////////////////\n\n
-`)
+
 
 
 //     uri: `https://eproc.opuscapita.com/josso/IDBUS-UI/IDP/SSO/LOGIN/SIMPLE?-1.IFormSubmitListener-signIn-signInForm&SSOArt=id${SSOArtID}`,
@@ -77,6 +72,7 @@ var pizda = ''
 var kakashka = ''
 var govno = ''
 var shluha = ''
+var piska = ''
 
 // Retrive first page to get Sessions
 var req = newAgent().get(eproc.uri('opc')).
@@ -92,20 +88,8 @@ then(response => {
         var j = new CookieJar(restCookies)
         j.add(preciousCookie)
 
-        // console.log('=======================================================>',SSOArtID, eproc.login,eproc.pass);
-        // console.log('=======================================================>',path);
-        // console.log('=======================================================>',`https://p2p.opuscapita.com/josso/IDBUS-UI/IDP/SSO/LOGIN/SIMPLE?-1.IFormSubmitListener-signIn-signInForm&SSOArt=id${SSOArtID}`);
-        // console.log('cookie =========================>', response.header['set-cookie'][0].substring(0,64));
+        console.log(` -> Sessions got, cookies:\n${j.string}\n`);
 
-        // https://p2p.opuscapita.com/josso/IDBUS-UI/IDP/SSO/LOGIN/SIMPLE?SSOArt=id4255FF912F78FA
-        // https://p2p.opuscapita.com/josso/IDBUS-UI/IDP/SSO/LOGIN/SIMPLE?-1.IFormSubmitListener-signIn-signInForm&SSOArt=id${SSOArtID}
-        // response.redirects[4]
-
-        // request.jar.setCookie()
-        // console.log('request ============> ', request.jar.getCookies());
-        // set('Cookie', response.header['set-cookie'][0].substring(0,64)). // .substring(0,64) 65
-
-        // request.jar.setCookie(preciousCookie,eproc.host, '/')
         var neu = newAgent();
 
         // Perform Auth
@@ -118,41 +102,41 @@ then(response => {
         send('passwordFormGoup%3Apassword=Peter.Bedarf').
         send('language=en').
         then(response => {
-            lol = response;
-            // console.log(response)
+            console.log(' -> Authenticated\n')
 
-            var preciousCookies = response.request.cookies
+            // var preciousCookies = response.request.cookies
             j.add(preciousCookie)
+
+
+            var productKey = 'lap001_910-001246'
+            var shortDescription = 'USB Mouse'
+            // Can be needed to perform a search before that !!!!!!
 
             // Add to cart
             neu.
             post(eproc.uri(`opc/add2Cart/addToCartSimple?isAddFromSearch=true`)).
-            send('productKey=lap001_910-001246').
+            send('productKey=' + productKey).
             send('isExternal=false').
-            send('shortDesc=USB Mouse').
+            send('shortDesc=' + shortDescription).
             send('quantity=1').
             then(response => {
+                console.log(` -> Added to cart: '${shortDescription}' with product key: ${productKey}\n`)
+
+                // Getting cart info
                 hui = response
                 neu.
                 get(eproc.uri('opc/shoppingCart/index?backUrl=')).
                 then(response => {
-                    chlen=response
+
                     var html = response.res.text.toString()
                     var salesOrderName = html.match(/name="salesOrderName" value="([^"]+)"/)[1]
                     var domainObjectId = html.match(/name="id" value="([^"]+)"/)[1]
                     var salesOrderId = html.match(/name="salesOrderId" value="([^"]+)"/)[1]
                     var salesOrderItemKey = html.match(/name="salesOrderItemKey" value="([^"]+)"/)[1]
 
-                    console.log('CHLEN---------------------------------')
-                    console.log('opc/shoppingCart/order')
-                    console.log('salesOrderName=' + salesOrderName)
-                    console.log('id=' + domainObjectId)
-                    console.log('salesOrderId=' + salesOrderId)
-                    console.log('productKey=' + 'lap001_910-001246')
-                    console.log('salesOrderItemKey=' + salesOrderItemKey)
-                    console.log('quantity' + salesOrderItemKey + '=1')
+                    console.log(` -> Opened cart, gethered info:\nsalesOrderName:${salesOrderName}\ndomainObjectId:${domainObjectId}\nsalesOrderId:${salesOrderId}\nsalesOrderItemKey:${salesOrderItemKey}\n-------\n\n`)
 
-
+                    // Ordering
                     neu.
                     post(eproc.uri('opc/shoppingCart/order')).
                     send('salesOrderName=' + salesOrderName).
@@ -167,45 +151,35 @@ then(response => {
                     send('shortDesc=USB Mouse').
                     send('itemId=').
                     then(response => {
-                        pizda = response
-
                         var toSend = response.res.text.toString().match(/value='([^']+)'/)[1]
+                         console.log(` -> Order made, response: ${toSend}\n`)
                         
+                        // Gathering checkout info
                         neu.
                         post(eproc.uri('opc/checkout/show')).
-                        send('customValidationResult='+toSend).
+                        send('customValidationResult=' + toSend).
                         then(response => {
 
                             var html = response.res.text.toString()
 
-                            // <input type="hidden" name="shoppingCartName_0" value="Cart(2018-07-05)" id="shoppingCartName_0" />
-                            // <input type="hidden" name="itemKeys_0" value="1859,1863" id="itemKeys_0" />
-
                             var shoppingCartName = html.match(/name="shoppingCartName_0" value="([^"]+)"/)[1]
                             var itemKeys = html.match(/name="itemKeys_0" value="([^"]+)"/)[1]
 
+                            console.log(` -> Checkout info obtained:\nshoppingCartName:${shoppingCartName}\nitemKeys:${itemKeys}\n-------\n\n`)
 
-                            console.log('Govno---------------------------------')
-                            console.log('shoppingCartName_0=' + shoppingCartName)
-                            console.log('itemKeys_0=' + itemKeys)
-                            console.log('quantity' + salesOrderItemKey + '=1')
-                            console.log('checkout: Place an Order')
-
-                            govno = html
-
-                            var eprocRequest = newAgent()
-                            eprocRequest.
+                            // Authenticating eProc
+                            var eprocAuthRequest = newAgent()
+                            eprocAuthRequest.
                             get(eproc.uri(`proc`)).
                             set('Cookie',  j.string)
                             .then(response => {
-                                shluha = response
                                 var newCookies = response.request.cookies.split(';').filter(cookie => !cookie.includes('JSESSIONID'))
 
                                 j.add(newCookies)
-                                console.log(j)
+                                console.log(` -> Authentification successfull, obtained cookies: ${newCookies.join(';')}\n`)
 
+                                // Confirming order
                                 enchancedRequest = newAgent();
-
                                 enchancedRequest.
                                 post(eproc.uri('opc/checkout/confirm')).
                                 set('Cookie', j.string).
@@ -214,7 +188,10 @@ then(response => {
                                 send('quantity' + salesOrderItemKey + '=1').
                                 send('checkout: Place an Order').
                                 then(response => {
-                                    kakashka = response
+
+                                    var requisitionId = response.res.req.path.match(/salesOrderId=([^/]+)/)[1]
+
+                                    console.log(` -> Order placed, requisitionId: ${requisitionId}\n`);
                                 })
 
                             })
